fa6ea67c4c4d56675f7edd31550f9d228f09a924
aglaia <thisisaglaia@gmail.com>
2026-03-11 12:28:23 +0100

    working on framework

--- search-problems/framework.hpp
+++ search-problems/framework.hpp
@@ -3,28 +3,145 @@ Il diagramma dovrà modellare concetti astratti come Problem, State, Node, Actio
 Potrebbe essere utile permettere all'utente (che definisce una sottoclasse di Problem) di specificare alcune caratteristiche note dello spazio degli stati, se queste possono essere utili all'algoritmo generico (ad es., il fatto che il modello di transizione è in realtà un albero, cosa che permetterebbe di evitare di ricordare gli stati visitati).
 */
 
-template <typename T, typename C>
-class Frontier {
-    protected:
-        C structure;
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
 
-    public:
-        void add(T item) {
-            structure.push(item);
-        }
+using namespace std;
 
-        T get() {
-            T item;
-            // distinguishes between structures that have the "front" method (queue)
-            // and the others (which use "top")
-            if constexpr (requires { structure.front(); }) { 
-                item = structure.front();
-            } else item = structure.top();
+class State {
 
-            structure.pop();
-            return item;
-        }
+public:
+    virtual ~State() = default;
+    virtual bool operator==(const State& other) const = 0;
+
+    virtual std::string to_string() const = 0;
+
 };
 
+class Action {
+public:
+    virtual ~Action() = default;
 
+    // for plan printing
+    virtual string get_name();
 
+};
+
+class Node {
+private:
+    Node* parent;
+    Action* action;
+    State* state;
+
+    int depth;
+
+    double g;
+    double h;
+
+public:
+    // constructor for root
+    Node(State* initial_state) {
+        this->parent = nullptr;
+        this->action = nullptr;
+        this->state = initial_state;
+        this->depth = 0;
+        this->g = 0.0;
+        this->h = 0.0;
+    }
+
+    // constructor for non-root
+    Node(Node* p, Action* a, State* s, double step_cost, double heur = 0.0) {
+        this->parent = p;
+        this->action = a;
+        this->state = s;
+        this->depth = p -> get_depth() + 1;
+        this->g = p -> gValue() + step_cost; 
+        this->h = heur;
+    }
+
+    State* get_state() const { return state; }
+    Node* get_parent() const { return parent; }
+    Action* get_action() const { return action; }
+
+    int get_depth() const { return depth; }
+
+    bool is_equivalent(Node* n) {
+        return *(this->state) == *(n->state);
+    }
+
+    double gValue() { return g; }
+    double hValue() { return h; }
+    double fValue() { return g + h; }
+
+};
+
+template <typename T, typename C>
+class Problem {
+
+protected:
+    State* initial_state;
+    bool is_tree;
+
+public: 
+    Problem(State* init, bool tree = false) {
+        this->initial_state = init;
+        this->is_tree = tree;
+    }
+
+    virtual ~Problem() = default;
+
+    State* get_initial_state() { return initial_state; }
+    bool is_tree_search() { return is_tree; }
+
+    virtual vector<Action*> get_actions(State* state) = 0;
+
+    virtual State* action_result(State* state, Action* action) = 0;
+    
+    // either a state equality check or something else
+    bool is_result(State* state) = 0;
+
+    virtual double step_cost(State* state, Action* action, State* new_state) {
+        return 1.0; 
+    }
+
+};
+
+class Frontier {
+protected:
+    std::vector<Node*> nodes;
+    std::unordered_map<std::string, Node*> stateIndexMap;
+
+public:
+    virtual ~Frontier() = default;
+
+    bool is_empty() {
+        return nodes.empty();
+    }
+
+    virtual void add(Node* node) {
+        nodes.push_back(node);
+        stateIndexMap[node->get_state()->to_string()] = node;
+    }
+
+    // implemented by subclasses based on policy
+    virtual Node* dequeue() = 0;
+
+    bool contains(State* state) { return stateIndexMap.find(state->to_string()) != stateIndexMap.end(); }
+
+    virtual void remove(Node* node) {
+        if (node == nullptr) return;
+        
+        // remove from hashmap
+        stateIndexMap.erase(node->get_state()->to_string());
+        
+        // remove from frontier
+        nodes.erase(std::remove(nodes.begin(), nodes.end(), node), nodes.end());
+    }
+
+    // to implement in subclasses: finds node in frontier, sees if it's better according
+    // to the chosen measure, and, if so, removes the old one and adds the new one
+    virtual void replace_if_better(Node* new_node);
+
+};
